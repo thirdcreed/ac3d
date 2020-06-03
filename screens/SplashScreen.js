a7f8d80874788546ef527e22f846c306c2b16f75
Christopher David <chris@arcade.city>
2020-06-03 14:05:10 -0500

    Add mock login screen (#2)
    * yarn add drei
    
    * Add sky to splash screen
    
    * Stars
    
    * Stars showing on mobile
    
    * Clean up splashscreen
    
    * Add textinput
    
    * Submit button and dummy submit handler

--- screens/SplashScreen.js
+++ screens/SplashScreen.js
@@ -1,39 +1,60 @@
-import React, { useRef, useState } from 'react'
-import { Canvas, useFrame } from 'react-three-fiber'
-
-function Box(props) {
-    // This reference will give us direct access to the mesh
-    const mesh = useRef()
-
-    // Set up state for the hovered and active state
-    const [hovered, setHover] = useState(false)
-    const [active, setActive] = useState(false)
-
-    // Rotate mesh every frame, this is outside of React without overhead
-    useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
-
-    return (
-        <mesh
-            {...props}
-            ref={mesh}
-            scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
-            onClick={(e) => setActive(!active)}
-            onPointerOver={(e) => setHover(true)}
-            onPointerOut={(e) => setHover(false)}
-        >
-            <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-            <meshStandardMaterial attach="material" color={hovered ? 'hotpink' : 'orange'} />
-        </mesh>
-    )
-}
+import React, { useState } from 'react'
+import { Button, TextInput, View } from 'react-native'
+import { THREE } from 'expo-three'
+import { Canvas } from 'react-three-fiber'
+import Stars from './Stars'
 
 export default function SplashScreen() {
+    const [value, onChangeText] = useState('Useless Placeholder')
+    const submit = () => {
+        alert('Submitted ' + value)
+    }
     return (
-        <Canvas>
-            <ambientLight />
-            <pointLight position={[10, 10, 10]} />
-            <Box position={[-1.2, 0, 0]} />
-            <Box position={[1.2, 0, 0]} />
-        </Canvas>
+        <View style={{ flex: 1 }}>
+            <Canvas
+                concurrent
+                gl={{ antialias: false }}
+                camera={{ position: [0, 0, 2000], near: 0.01, far: 10000, fov: 70 }}
+                onCreated={({ gl, camera }) => {
+                    gl.gammaInput = true
+                    gl.toneMapping = THREE.Uncharted2ToneMapping
+                    gl.setClearColor(new THREE.Color('#02020a'))
+                }}
+                style={{ backgroundColor: 'black' }}
+            >
+                <fog attach="fog" args={['#070710', 100, 700]} />
+                <ambientLight intensity={0.25} />
+                <Stars />
+            </Canvas>
+            <TextInput
+                style={{
+                    height: 60,
+                    borderColor: 'gray',
+                    borderWidth: 1,
+                    backgroundColor: 'rgba(0,0,0,0.5)',
+                    color: '#fff',
+                    padding: 12,
+                    fontSize: 18,
+                    position: 'absolute',
+                    top: 350,
+                    left: 40,
+                    width: '80%'
+                }}
+                onChangeText={(text) => onChangeText(text)}
+                value={value}
+            />
+            <View style={{ position: 'absolute', top: 420, left: 160 }}>
+                <Button
+                    onPress={submit}
+                    style={{
+                        backgroundColor: '#999',
+                        color: '#fff',
+                        height: 40,
+                        width: 340
+                    }}
+                    title="Submit"
+                />
+            </View>
+        </View>
     )
 }
