9f1e695ba6772bdb16c6ad4d5eaabb8464dc48fe
Christopher David <chris@arcade.city>
2020-06-03 10:02:19 -0500

    Setup three.js and splashscreen w/ R3F box example (#1)
    * yarn.lock
    
    * prettier
    
    * React navigation remove root path
    
    * Add splash route
    
    * Hide header globally
    
    * yarn add three expo-three
    
    * yarn add expo-gl expo-three three@latest react-three-fiber@beta
    
    * Add three.js with box example
    
    * Stfu performance

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -1,48 +1,64 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import * as React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import * as React from 'react'
 
-import TabBarIcon from '../components/TabBarIcon';
-import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
+import TabBarIcon from '../components/TabBarIcon'
+import SplashScreen from '../screens/SplashScreen'
+import HomeScreen from '../screens/HomeScreen'
+import LinksScreen from '../screens/LinksScreen'
 
-const BottomTab = createBottomTabNavigator();
-const INITIAL_ROUTE_NAME = 'Home';
+const BottomTab = createBottomTabNavigator()
+const INITIAL_ROUTE_NAME = 'Splash'
 
 export default function BottomTabNavigator({ navigation, route }) {
-  // Set the header title on the parent stack navigator depending on the
-  // currently active tab. Learn more in the documentation:
-  // https://reactnavigation.org/docs/en/screen-options-resolution.html
-  navigation.setOptions({ headerTitle: getHeaderTitle(route) });
+    // Set the header title on the parent stack navigator depending on the
+    // currently active tab. Learn more in the documentation:
+    // https://reactnavigation.org/docs/en/screen-options-resolution.html
+    navigation.setOptions({ headerTitle: getHeaderTitle(route) })
 
-  return (
-    <BottomTab.Navigator initialRouteName={INITIAL_ROUTE_NAME}>
-      <BottomTab.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{
-          title: 'Get Started',
-          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-code-working" />,
-        }}
-      />
-      <BottomTab.Screen
-        name="Links"
-        component={LinksScreen}
-        options={{
-          title: 'Resources',
-          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-book" />,
-        }}
-      />
-    </BottomTab.Navigator>
-  );
+    return (
+        <BottomTab.Navigator initialRouteName={INITIAL_ROUTE_NAME}>
+            <BottomTab.Screen
+                name="Splash"
+                component={SplashScreen}
+                options={{
+                    headerShown: false, // ??
+                    title: 'Splashhhh',
+                    tabBarIcon: ({ focused }) => (
+                        <TabBarIcon focused={focused} name="md-code-working" />
+                    )
+                }}
+            />
+            <BottomTab.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{
+                    title: 'Get Started',
+                    tabBarIcon: ({ focused }) => (
+                        <TabBarIcon focused={focused} name="md-code-working" />
+                    )
+                }}
+            />
+            <BottomTab.Screen
+                name="Links"
+                component={LinksScreen}
+                options={{
+                    title: 'Resources',
+                    tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-book" />
+                }}
+            />
+        </BottomTab.Navigator>
+    )
 }
 
 function getHeaderTitle(route) {
-  const routeName = route.state?.routes[route.state.index]?.name ?? INITIAL_ROUTE_NAME;
+    const routeName = route.state?.routes[route.state.index]?.name ?? INITIAL_ROUTE_NAME
 
-  switch (routeName) {
-    case 'Home':
-      return 'How to get started';
-    case 'Links':
-      return 'Links to learn more';
-  }
+    switch (routeName) {
+        case 'Splash':
+            return 'Welcome to Arcade City'
+        case 'Home':
+            return 'How to get started'
+        case 'Links':
+            return 'Links to learn more'
+    }
 }
--- navigation/LinkingConfiguration.js
+++ navigation/LinkingConfiguration.js
@@ -1,14 +1,15 @@
-import * as Linking from 'expo-linking';
+import * as Linking from 'expo-linking'
 
 export default {
-  prefixes: [Linking.makeUrl('/')],
-  config: {
-    Root: {
-      path: 'root',
-      screens: {
-        Home: 'home',
-        Links: 'links',
-      },
-    },
-  },
-};
+    prefixes: [Linking.makeUrl('/')],
+    config: {
+        Root: {
+            path: '/',
+            screens: {
+                Splash: 'splash',
+                Home: 'home',
+                Links: 'links'
+            }
+        }
+    }
+}
